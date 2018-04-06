465de6e4a4b12d5ad5102a79ab723ed9437a01fc
Jordan Visaggio <jvisaggio@sae.au>
2018-04-06 17:40:22 +0800

    Signed-off-by: Jordan Visaggio <jvisaggio@sae.au>

---
+++ JBTowerDefence/Assets/Scripts/TurretPlacement/TurretController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurretController : MonoBehaviour {
+    [SerializeField]
+    Button[] towerButtons;
+    
+    bool isPlacing = false;
+    public GameObject tower1;
+    enum SelectState {
+        WaitForSelection,
+        Tower1Placement
+    };
+    SelectState currentState = SelectState.WaitForSelection;
+
+    void Start() {
+    }
+    void Update() {
+        switch (currentState) {
+            default:
+                WaitForSelection();
+                break;
+            case SelectState.WaitForSelection:
+                WaitForSelection();
+                break;
+            case SelectState.Tower1Placement:
+                TowerPlacement();
+                break;
+        }
+    }
+    void WaitForSelection() {
+        for (int i = 0; i < towerButtons.Length; i++) {
+            towerButtons[i].interactable = true;
+            towerButtons[i].gameObject.SetActive(true);
+        }
+    }
+
+    void TowerPlacement() {
+        RaycastHit hit = new RaycastHit();
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out hit, Mathf.Infinity);
+        if (hit.collider != null && isPlacing) {
+            Debug.Log(hit.collider.gameObject);
+            if (Input.GetKeyDown(KeyCode.Mouse0)) {
+                Instantiate(tower1, hit.collider.gameObject.transform.position, Quaternion.identity);
+                currentState = SelectState.WaitForSelection;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1) && isPlacing) {
+            isPlacing = false;
+            currentState = SelectState.WaitForSelection;
+        }
+    }
+    public void SwitchToTowerPlacement(int towerType) {
+        for (int i = 0; i < towerButtons.Length; i++) {
+            towerButtons[i].interactable = false;
+            towerButtons[i].gameObject.SetActive(false);
+        }
+        isPlacing = true;
+        if (towerType == 1) {
+        currentState = SelectState.Tower1Placement;
+        }
+    }
+}
