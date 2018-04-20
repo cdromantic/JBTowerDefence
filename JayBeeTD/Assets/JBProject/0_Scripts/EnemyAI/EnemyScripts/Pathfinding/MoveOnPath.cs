64a60a89380f6edd9d88790557f117195220db05
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-04-20 16:51:09 +0800

    Merged all, fixed some issues
    Merged all member projects. fixed some merged compile issues
    compiled and organised scripts, prefabs, etc. please use this for work

---
+++ JayBeeTD/Assets/JBProject/0_Scripts/EnemyAI/EnemyScripts/Pathfinding/MoveOnPath.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnPath : MonoBehaviour {
+    [SerializeField]
+    JayBeeHealth jBHP;
+    GameObject pathsHolder;
+    Paths followPath;
+    EnemyWaveSpawn eWS;
+
+    public int currentWayPoint = 0;
+    public float speed = 1;
+    public float reachDist = 0.25f;
+    public float rotateSpeed = 0.5f;
+
+    Vector3 lastPos;
+    Vector3 currentPos;
+
+    void Start() {
+        pathsHolder = GameObject.FindGameObjectWithTag("WaveSpawner");
+        eWS = pathsHolder.GetComponent<EnemyWaveSpawn>();
+        followPath = pathsHolder.GetComponent<Paths>();
+        lastPos = transform.position;
+    }
+
+    void Update() {
+        if (currentWayPoint != followPath.pathObject.Count) {
+            //this makes the enemy move towards the path
+            Debug.Log(followPath.pathObject.Count);
+            float dist = Vector3.Distance(followPath.pathObject[currentWayPoint].position, transform.position);
+            transform.position = Vector3.MoveTowards(transform.position, followPath.pathObject[currentWayPoint].position, Time.deltaTime * speed);
+
+            if (dist <= reachDist) {
+                lastPos = followPath.pathObject[currentWayPoint].position;
+                currentWayPoint++;
+            }
+        }
+
+        else {
+            jBHP.Damage();
+            eWS.RemoveEnemyFromList(gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ JayBeeTD/Assets/JBProject/0_Scripts/JBAI/JayBeeHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JayBeeHealth : MonoBehaviour {
+
+    public float jBHealth = 5f;
+    public float jBMaxHealth = 5f;
+
+    public bool jBIsDead;
+    void Start() {
+        jBHealth = jBMaxHealth;
+    }
+
+    void Update() {
+
+    }
+    public void Damage() {
+        jBHealth -= 1f;
+        if (jBHealth <= 0) {
+            if (jBIsDead) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
