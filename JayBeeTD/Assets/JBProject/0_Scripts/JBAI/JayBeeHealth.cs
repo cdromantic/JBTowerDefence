561cfaeee631ecc193dac0a05c3f247bcb88fba3
cdromantic <jordanvisaggio@gmail.com>
2018-04-27 13:52:52 +0800

    game over, shop added

--- JayBeeTD/Assets/JBProject/0_Scripts/JBAI/JayBeeHealth.cs
+++ JayBeeTD/Assets/JBProject/0_Scripts/JBAI/JayBeeHealth.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class JayBeeHealth : MonoBehaviour {
+    [SerializeField]
+    private Canvas gameOver;
+    [SerializeField]
+    private AudioSource gameOverTheme;
+    [SerializeField]
+    private AudioSource bgm;
 
     public float jBHealth = 5f;
     public float jBMaxHealth = 5f;
@@ -10,6 +16,8 @@ public class JayBeeHealth : MonoBehaviour {
     public bool jBIsDead;
     void Start() {
         jBHealth = jBMaxHealth;
+        Time.timeScale = 1f;
+        gameOver.enabled = false;
     }
 
     void Update() {
@@ -20,8 +28,10 @@ public class JayBeeHealth : MonoBehaviour {
         if (jBHealth < 1f) {
             jBIsDead = true;
             if (jBIsDead) {
-                //endgamescript();
-                Destroy(gameObject);
+                Time.timeScale = 0f;
+                gameOver.enabled = true;
+                bgm.GetComponent<AudioSource>().Pause();
+                gameOverTheme.GetComponent<AudioSource>().Play();
             }
         }
     }
